a2754b14121d8cc5cb6ff442c5ec05d9a5e7470c
Makulov Evgeny <makulovev@gmail.com>
2022-07-22 00:09:43 +0300

    Add stack and workinig things

--- hw22.playground/Contents.swift
+++ hw22.playground/Contents.swift
@@ -1,12 +1,8 @@
-//: A UIKit based Playground for presenting user interface
-  
 import UIKit
 import PlaygroundSupport
 import Foundation
 
 //Chip
-import Foundation
-
 public struct Chip {
     public enum ChipType: UInt32 {
         case small = 1
@@ -32,18 +28,18 @@ public struct Chip {
 
 //Stack
 class Stack {
-
+    
     private var stack = [Chip]()
     private let concurrentQueue = DispatchQueue(label: "concurrent-queue", qos: .utility, attributes: .concurrent)
     var count: Int { stack.count }
-
+    
     func addChip(_ chip: Chip) {
         concurrentQueue.async(flags: .barrier) { [unowned self] in
             self.stack.append(chip)
             print ("Микросхема размером \(chip.chipType) взята на обработку. Остаток: \(getAllChips())")
         }
     }
-
+    
     func grabChip() -> Chip? {
         var chip: Chip?
         concurrentQueue.sync { [unowned self] in
@@ -53,8 +49,71 @@ class Stack {
         }
         return chip
     }
-
+    
     func getAllChips() -> [UInt32] {
         stack.compactMap { $0.chipType.rawValue }
     }
 }
+
+//Thread
+class WorkingThread: Thread {
+    
+    private var stack: Stack
+    private let count: Int
+    private let interval: Double
+    
+    init(stack: Stack, count: Int = 10, interval: Double = 2) {
+        self.stack = stack
+        self.count = count
+        self.interval = interval
+    }
+    
+    override func main() {
+        for _ in 1...count {
+            let chip = createChip()
+            stack.addChip(chip)
+            Thread.sleep(forTimeInterval: interval)
+        }
+        cancel()
+        print("WorkingThread cancel")
+    }
+    
+    private func createChip() -> Chip {
+        let chip = Chip.make()
+        print("\nМикросхема размера \(chip.chipType) поступила. Остаток: \(stack.getAllChips())")
+        return chip
+    }
+}
+
+//Solder
+class SolderThread: Thread {
+    
+    private var stack: Stack
+    
+    init(stack: Stack) { self.stack = stack }
+    
+    override func main() {
+        while stack.count > 0 || !workingThread.isCancelled {
+            doWork()
+        }
+        cancel()
+        print("SolderThread cancel")
+    }
+    
+    private func doWork() {
+        guard let chip = stack.grabChip() else { return }
+        solderChip(chip)
+    }
+    
+    private func solderChip(_ chip: Chip) {
+        chip.sodering()
+        print ("Микросхема размером \(chip.chipType) припаяна. Остаток: \(stack.getAllChips())")
+    }
+}
+
+//Work
+let stack = Stack()
+let workingThread = WorkingThread(stack: stack, interval: 1)
+let solderThread = SolderThread(stack: stack)
+workingThread.start()
+solderThread.start()
